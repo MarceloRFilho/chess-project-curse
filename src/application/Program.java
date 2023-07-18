aee638cd9990349e347e21c755720d5f0fda4745
Marcelo R Filho <marcelo.1496@gmail.com>
2023-07-18 11:40:20 -0400

    Board

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,18 +2,14 @@ package application;
 
 import java.util.Scanner;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		
-		Position pos = new Position(4, 5);
-		
-		System.out.println(pos);
-		
+		Board board = new Board(8, 8);
 		
 		sc.close();
 		
